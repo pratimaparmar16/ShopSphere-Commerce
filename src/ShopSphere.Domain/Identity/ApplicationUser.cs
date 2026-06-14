9f9708e55a87ee4cf943bf5294e7c34810b2f8bc
pratimaparmar <princyparmar1625@gmail.com>
2026-06-14 21:37:20 +0530

    feat(identity): add application user entity

---
+++ src/ShopSphere.Domain/Identity/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ShopSphere.Domain.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        } = string.Empty;
+
+        public string LastName
+        {
+            get;
+            set;
+        } = string.Empty;
+    }
+}
